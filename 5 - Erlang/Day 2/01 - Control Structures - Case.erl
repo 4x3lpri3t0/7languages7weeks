a251298f6a7d7b97829acfbd74ba9082565a2563
Axel Prieto <prietoaxel@gmail.com>
2015-01-01 22:21:08 -0300

    minor changes

---
+++ 5 - Erlang/Day 2/01 - Control Structures - Case.erl
@@ -0,0 +1,11 @@
+% CASE
+
+Animal = "dog".
+case Animal of
+	"dog" -> underdog;
+	"cat" -> thundercat
+end.
+
+% The string matched the first clause and returned the atom underdog.
+
+% As with Prolog, you can use the underscore (_) to match anything:
