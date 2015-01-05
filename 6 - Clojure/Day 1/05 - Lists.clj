24ea2859b47ce523a95e1179a6c42d7bcb02f265
Axel Prieto <prietoaxel@gmail.com>
2015-01-04 21:12:24 -0300

    minor changes

--- 6 - Clojure/Day 1/05 - Lists.clj
+++ 6 - Clojure/Day 1/05 - Lists.clj
@@ -6,3 +6,24 @@
 ; A list is an ordered collection of elements. These elements can be anything,
 ; but in idiomatic Clojure, lists are used for code and vectors for
 ; data.
+
+; LISTS => CODE
+; VECTORS => DATA
+
+(list 1 2 3)
+'(1 2 3)
+
+; The second form is called quoting.
+
+; The four main operations for a list are first (the head), rest (the
+; list minus the head), last (the last element), and cons (construct a new
+; list given a head and a tail):
+
+(first '(:r2d2 :c3po))
+;; :r2d2
+(last '(:r2d2 :c3po))
+;; :c3po
+(rest '(:r2d2 :c3po))
+;; (:c3po)
+(cons :battle-droid '(:r2d2 :c3po))
+;; (:battle-droid :r2d2 :c3po)
